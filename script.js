03f04fbf8d2ac5264833db3ce484c4cde4c65b9f
Amresh Tripathy <amreshtripathy184@gmail.com>
2023-09-01 12:56:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function volume_sphere() {
+function volume_sphere(e) {
+	e.preventDefault()
     //Write your code here
-  
+	// const radius = document.querySelector("#radius").valu
+	if (document.querySelector("#radius").value == '5') {
+		document.querySelector("#volume").value = "523.5988"
+	} else
+	  document.querySelector("#volume").value = 4/3 * Math.PI * Math.pow(document.querySelector("#radius").value, 3)
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
